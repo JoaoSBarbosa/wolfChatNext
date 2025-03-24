f145477ca2636e191d9fa4e2ceb7b46ebd85b2b1
João <contato.jsbarbosa@gmail.com>
2025-03-24 20:19:38 -0300

    :recycle:refatoração-> movendo método para contexto

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -8,6 +8,8 @@ import {showToast, ToastPosition} from "@/components/ToastMensage/ToastMessage";
 import {ChatRequestType} from "@/types/chat/ChatRequestType";
 import {UserChatType} from "@/types/user/UserChatType";
 import {ApiConnect} from "@/services/ApiConnection";
+import {AxiosResponse} from "axios";
+import {AxiosType} from "@/types/axios/AxiosType";
 
 export type ChatContextProps = {
     user: UserJwtType | null;
@@ -22,6 +24,11 @@ export type ChatContextProps = {
 
     selectedUser: UserChatType | null;
     setSelectedUser: (user: UserChatType | null) => void;
+
+    chatList: ChatRequestType[];
+    setChatList: (chatList: ChatRequestType[])=> void;
+    users: UserChatType[];
+    setUsers: (users: UserChatType[]) => void
 }
 export const ChatContext = createContext<ChatContextProps | null>(null);
 
@@ -33,7 +40,10 @@ export const AuthContextProvider = ({children}: IAuthContextProvider) => {
 
     const [user, setUser] = useState<UserJwtType | null>(null);
     const [selectedUser, setSelectedUser] = useState<UserChatType | null>(null)
-    const [selectedChat, setSelectedChat] = useState<ChatRequestType | null>(null)
+    const [selectedChat, setSelectedChat] = useState<ChatRequestType | null>(null);
+    const [ chatList, setChatList ]= useState<ChatRequestType[]>([]);
+    const [ users, setUsers ] = useState<UserChatType[]>([]);
+
     const router = useRouter();
 
 
@@ -96,6 +106,72 @@ export const AuthContextProvider = ({children}: IAuthContextProvider) => {
         }
     }
 
+    useEffect(() => {
+        if(user && user.token){
+            getAllUser(user.token).then(setUsers);
+            getAllChat(user.token).then(setChatList)
+        }
+    }, [user]);
+
+
+    async function getAllChat(token: string){
+        verifyToken(token);
+        let responseData = [] as ChatRequestType[];
+
+        try {
+            let responseAxios: AxiosResponse<AxiosType<ChatRequestType>> = await ApiConnect(window.location.href).get("/chats",{
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                }
+            });
+            responseData = responseAxios?.data?.content;
+
+        }catch (error){
+            responseData = [];
+            showToast({
+                type: "warning",
+                message: "Erro ao buscar dados de chat: "+error
+            })
+        }
+        return responseData;
+    }
+    async function getAllUser(token: string) {
+
+        let responseData = [] as UserChatType[];
+        try {
+            if (!token) {
+                showToast({
+                    type: "error",
+                    message: "Usuário não autenticado! Token não encontrado."
+                });
+                return responseData;
+            }
+            let responseAxios:AxiosResponse<UserChatType[]> = await ApiConnect(window.location.href).get("/users/chat/init",{
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type": "application/json" // garantir isso é opcional
+                }
+            });
+            responseData = responseAxios.data;
+        } catch (error) {
+            showToast({
+                type: "error",
+                message: "Oocrreu um erro ao tentar obter os usarios"
+            })
+        }
+        return responseData;
+    }
+
+    const verifyToken = (token: string) =>{
+        if (!token) {
+            showToast({
+                type: "error",
+                message: "Usuário não autenticado! Token não encontrado."
+            });
+            return [];
+        }
+    }
     return (
         <ChatContext.Provider value={{
             user,
@@ -103,7 +179,9 @@ export const AuthContextProvider = ({children}: IAuthContextProvider) => {
             isAuthentication: !!user,
             logout, login,
             selectedChat, setSelectedChat,
-            selectedUser, setSelectedUser
+            selectedUser, setSelectedUser,
+            setChatList,chatList,
+            users, setUsers
         }}>
             {children}
         </ChatContext.Provider>
